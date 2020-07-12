84658adf2a5de66da401920b8b3ebb989f1a86bd
PeterDeclerck82 <peter.declerck2@student.howest.be>
2020-07-12 17:17:59 +0200

    Add practical jQuery.

--- WorldResume/wwwroot/js/index.js
+++ WorldResume/wwwroot/js/index.js
@@ -1,20 +1,34 @@
-﻿var x = 0;
-var s = "";
+﻿$(document).ready(function () {         // .ready = deze function word enkel opgeroepen als de volledige
+                                        // pagina is geladen
+
+    var x = 0;
+    var s = "";
 
 
-var theForm = $("#theForm");        // = document.getElementById("theForm");
-                                    // " $ " staat voor het gebruik van jQuery
-theForm.hide();
+    var theForm = $("#theForm");        // = document.getElementById("theForm");
+    // " $ " staat voor het gebruik van jQuery
+    theForm.hide();
 
 
-var button = $("#addButton");       // = document.getElementById("addButton");
-button.on("click", function () {    // = button.addEventListener("click", function () {
-    console.log("Adding item");
+    var button = $("#addButton");       // = document.getElementById("addButton");
+    button.on("click", function () {    // = button.addEventListener("click", function () {
+        console.log("Adding item");
+    });
+
+
+
+    var productInfo = $(".product-props li");      //document.getElementByClassName("product-props");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(500);                //als het zichtbaar is, uit; en omgekeerd
+    });
+
 });
-
-
-
-var productInfo = $(".product-props li");      //document.getElementByClassName("product-props");
-productInfo.on("click", function () {
-    console.log("You clicked on " + $(this).text());
-})
