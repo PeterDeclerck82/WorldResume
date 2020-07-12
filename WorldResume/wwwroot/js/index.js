91804a49fb421f21369e8159fdd202347377c912
PeterDeclerck82 <peter.declerck2@student.howest.be>
2020-07-12 14:53:53 +0200

    Add JavaScript Events.

--- WorldResume/wwwroot/js/index.js
+++ WorldResume/wwwroot/js/index.js
@@ -4,3 +4,11 @@ var s = "";
 var theForm = document.getElementById("theForm");
 
 theForm.hidden = false;
+
+var button = document.getElementById("addButton");
+button.addEventListener("click", function () {
+    console.log("Adding item");
+});
+
+var productInfo = document.getElementByClassName("product-props");
+var listItems = productInfo.item[0].children
