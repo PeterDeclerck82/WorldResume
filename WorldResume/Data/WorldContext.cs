21d6d5467a8572cd105f0f3feb85729915e7ead5
PeterDeclerck82 <peter.declerck2@student.howest.be>
2020-07-19 16:16:44 +0200

    Build database: create entities.

---
+++ WorldResume/Data/WorldContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorldResume.Data.Entities;
+
+namespace WorldResume.Data
+{
+    public class WorldContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+    }
+}
