06c05f75636ec48b487d8520dd8d0a7083ac7bfd
PeterDeclerck82 <peter.declerck2@student.howest.be>
2020-07-19 16:28:16 +0200

    Build database: use EF Core tooling.

--- WorldResume/Startup.cs
+++ WorldResume/Startup.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WorldResume.Data;
 
 namespace WorldResume
 {
@@ -16,6 +18,11 @@ namespace WorldResume
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<WorldContext>(cfg =>
+            {
+                cfg.UseSqlServer("");
+            });
+
             //services.AddTransient<>
             services.AddControllersWithViews();
         }
