c69b5aeca30033ab9e3323d9ed6d08c1ae914106
PeterDeclerck82 <peter.declerck2@student.howest.be>
2020-07-23 21:52:21 +0200

    Add repositories to the Model Class.

---
+++ WorldResume/Models/IContinentRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldResume.Models
+{
+    public interface IContinentRepository
+    {
+        IEnumerable<Continent> AllContinents { get; }
+    }
+}
---
+++ WorldResume/Models/ICountryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldResume.Models
+{
+    public interface ICountryRepository
+    {
+        IEnumerable<Country> AllCountries { get; }
+        IEnumerable<Country> CountriesOfTheWeek { get; }
+        Country GetCountryById(int countryId);
+    }
+}
